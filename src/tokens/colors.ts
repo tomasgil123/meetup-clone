cdd191cbfc8973c04604e904c8f8f7c124f7c7be
Tomas Gil <tomasgil1234@gmail.com>
2020-10-19 22:01:25 -0300

    feat: add tokens

---
+++ src/tokens/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+  base: {
+    white: '#ffffff',
+    borders: '#ebeff3',
+    primaryGreen: '#2a5913',
+    secondaryGreen: '#ADE8C4',
+    secondaryBlue: '#47657f',
+  },
+  text: {
+    error: '#DF744A',
+    link: '#4998d7',
+    success: '#57bc90',
+    primary: '#373b3e',
+    secondary: '#606b76',
+  },
+}
+
+export default colors
---
+++ src/tokens/index.ts
@@ -0,0 +1,6 @@
+import colors from './colors'
+import space from './space'
+import breakpoints from './breakpoints'
+import boxShadow from './boxShadow'
+
+export { colors, space, breakpoints, boxShadow }

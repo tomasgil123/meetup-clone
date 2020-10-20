f588c6ab9aaedb2c0c3cc511b0b616cdcc3d6e4a
Tomas Gil <tomasgil1234@gmail.com>
2020-10-19 22:30:28 -0300

    feat: add footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import { space, breakpoints, colors } from 'src/tokens'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  background-color: ${colors.base.purple};
+  border-top: 1px solid ${colors.base.borders};
+  padding-top: ${space.s6};
+  padding-bottom: ${space.s6};
+  padding-left: ${space.s12};
+  padding-right: ${space.s12};
+  align-items: center;
+  @media (min-width: ${breakpoints.md}) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`
+
+const Brand = styled.div`
+  color: ${colors.base.purple};
+`
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Brand>© TomasGil 2020</Brand>
+    </Container>
+  )
+}
+
+export default Footer
